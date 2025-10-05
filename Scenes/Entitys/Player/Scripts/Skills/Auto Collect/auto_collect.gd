949f8816dbf586e0e4925fc370e27a76f3b125a2
Francis Leo <drypasta34@gmail.com>
2025-10-05 16:55:32 +0800

    Update
    refactor: Fix the readability, maintainability, and scalability

---
+++ Scenes/Entitys/Player/Scripts/Skills/Auto Collect/auto_collect.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@onready var player = $"../.."
+@onready var G = Global
+
+func _process(delta: float) -> void:
+	if G.auto_collect:
+		var target = get_nearest_coin()
+		if target:
+			player.position = player.position.move_toward(target.global_position, G.speed * delta)
+
+func get_nearest_coin():
+	var nearest_area = null
+	var nearest_distance = INF
+	
+	for area in get_tree().get_nodes_in_group("coin"):
+		if area and area.is_inside_tree():
+			var distance = player.position.distance_to(area.global_position)
+			if distance < nearest_distance:
+				nearest_distance = distance
+				nearest_area = area
+	return nearest_area
+	
--- Scenes/Entitys/Player/Scripts/player.gd
+++ Scenes/Entitys/Player/Scripts/player.gd
@@ -3,29 +3,11 @@ extends CharacterBody2D
 @onready var G = Global
 
 func _physics_process(_delta: float) -> void:
-	if Global.auto_collect:
-		var target = get_nearest_coin()
-		if target:
-			position = position.move_toward(target.global_position, G.speed * _delta)
-
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	velocity = direction * G.speed
 	move_and_slide()
 	
+	#Limit player so that it cannot go outside the window
 	var screen_size = get_viewport_rect().size
-	
 	position.x = clamp(position.x, 20, (screen_size.x - 20))
 	position.y = clamp(position.y, 85, (screen_size.y - 20))
-
-func get_nearest_coin():
-	var nearest_area = null
-	var nearest_distance = INF
-	
-	for area in get_tree().get_nodes_in_group("coin"):
-		if area and area.is_inside_tree():
-			var distance = global_position.distance_to(area.global_position)
-			if distance < nearest_distance:
-				nearest_distance = distance
-				nearest_area = area
-		
-	return nearest_area
